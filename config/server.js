4dde4567d859c62dbe950c5a59f9574463a36f95
Waheed Aishat A <waheedaishat@gmail.com>
2023-02-25 03:55:40 +0100

    fix

--- config/server.js
+++ config/server.js
@@ -2,7 +2,7 @@ module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
-    keys: env.array('APP_KEYS', ["WmBZmklkNZY6u1J10QRidw==", "9D5yTEEWORQb+VewUT9AfQ==" , "A0rOq0jrCu1zfNgsmGet/A==,toi6UYzBGgWBmxnBcXARTA=="]),
+    keys: env.array('APP_KEYS'),
   },
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
