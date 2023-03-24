ad6abb7d0e5366cc1dfdbfae5b5b42d1766a2286
Waheed Aishat A <waheedaishat@gmail.com>
2023-03-24 10:48:30 +0100

    fix

--- config/plugins.js
+++ config/plugins.js
@@ -5,5 +5,16 @@ module.exports = ({ env }) => ({
     config: {
       jwtSecret: env('JWT_SECRET') || crypto.randomBytes(16).toString('base64'),
     },
+    "vercel-deploy": {
+      enabled: true,
+      config: {
+        deployHook:
+          "https://api.vercel.com/v1/integrations/deploy/prj_xmMr8UDmGRy0FpSuSOP6d1zBi0iG/ECnrIFOh4A",
+        apiToken: "dXDmpAcLBrIXVblcrDtYIGlx",
+        appFilter: "bag-shop-backend",
+        // teamFilter: "your-team-id-on-vercel",
+        roles: ["strapi-super-admin", "strapi-editor", "strapi-author"],
+      },
+    },
   },
 });
